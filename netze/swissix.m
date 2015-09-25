aec9ec643f293840518e6f1b8bf6d7a9e0dbac63
daniw <dani.winz@bluewin.ch>
2015-09-25 16:31:29 +0200

    Swissix

--- netze/swissix.m
+++ netze/swissix.m
@@ -1,14 +1,18 @@
 base = 1;
 peak = 2;
-%       base    peak
-s = [   4       4.7;    % 18.9.2015
-        4.54    4.88    % 24.9.2015
+%       base    peak    day     date
+s = [   4       4.7     1    ;  % 18.9.2015
+        4.54    4.88    7    ;  % 24.9.2015
+        4.66    5.1     8    ;  % 25.9.2015
 ];
 
+datestr(736224 + s(:,3))
+
 figure(1);
-plot(s);
+plot(s(:,3), s(:,1), s(:,3), s(:,2));
 title('Swissix');
 xlabel('Zeit');
 ylabel('Preis [Rp/kWh]');
+datetick();
 legend('base', 'peak');
 print -dpdf 'swissix'
