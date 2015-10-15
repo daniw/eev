a83c15c66373aad500c31883320c157dfc18ee16
daniw <dani.winz@bluewin.ch>
2015-10-15 16:17:03 +0200

    Korrekturen Leistungsberechnung 4

--- kraftwerke/sw03/leistungsberechnung4.m
+++ kraftwerke/sw03/leistungsberechnung4.m
@@ -2,7 +2,7 @@
 g = 9.81;
 dens = 1000;
 % Bruttohöhe
-hbr =  95;
+hbr =  93;
 % Wasserabfluss
 v = 6000;
 % Wirkungsgrad
@@ -10,7 +10,7 @@ eta = 0.93;
 % gemessene Leistung
 p_mess = 5000*1e6;
 % Betriebsstunden pro Jahr
-t = 5800;
+t = 5800 * 3600;
 % Berechnungen
 p = v * dens * g * hbr;
 p_mw = p / 1e6;
@@ -19,7 +19,7 @@ disp(strcat('reale Leistung: ', num2str(p_mw * eta), 'MW'));
 eta_real = p_mess / p;
 disp(strcat('tatsächlicher Wirkungsgrad: ', num2str(eta_real), ''));
 
-w = p * t * 3600;
+w = p_mess * t * 3600;
 w_gj = w / 1e9;
 w_mwh = w / 1e6 / 3600;
 disp(strcat('Jährliche Arbeit: ', num2str(w_gj), 'GJ'));
