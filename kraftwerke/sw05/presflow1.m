bec514709aab443e31dc982322719cce05ed5eb7
daniw <dani.winz@bluewin.ch>
2015-10-15 16:25:55 +0200

    Notes SW05

---
+++ kraftwerke/sw05/presflow1.m
@@ -0,0 +1,13 @@
+%% Konstanten
+g = 9.81;
+dens = 1000;
+%% Parameter
+% Bruttohöhe
+hbr = 800;
+% Bruttoleistung pro Maschine
+pbr = 60E6;
+% Anzahl Maschinen
+n_gen = 6;
+% Winkel zwischen Kanal und Druckleitung
+angle = 120;
+
