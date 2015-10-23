12708b35035fbfcdc69a35e250271bcd04c63642
daniw <dani.winz@bluewin.ch>
2015-10-23 14:22:49 +0200

    Wirkungsgrad 1 korrigiert

--- kraftwerke/sw06/wirkungsgrad1.m
+++ kraftwerke/sw06/wirkungsgrad1.m
@@ -11,7 +11,7 @@ h_uw = 193.5;
 q = 135;
 
 pv_bar = 0.175;
-h_v = 9.81 * pv_bar;
+h_v = pv_bar * 10^5 / rho / g;
 eta_gen = 0.95;
 eta_turb = 0.99;
 eta_saugrohr = 1.00;
@@ -29,8 +29,11 @@ p_nat_mw = p_nat / 10^6
 p_disp = q * rho * g * h_net;
 p_disp_mw = p_disp / 10^6
 
-p_turb = p_disp * eta_spalt * eta_saugrohr * eta_hyd * eta_turb;
+%p_turb = p_disp * eta_spalt * eta_saugrohr * eta_turb * eta_hyd;
+p_turb = p_disp * eta_spalt * eta_saugrohr * eta_turb;
 p_turb_mw = p_turb / 10^6
 
 p_gen = p_turb * eta_gen;
 p_gen_mw = p_gen / 10^6
+s_gen = p_gen / cosphi;
+s_gen_mva = s_gen / 10^6
